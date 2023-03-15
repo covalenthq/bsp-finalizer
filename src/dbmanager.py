3a049ddc7d673f6dc0158e32c3b16f0bfb6b9c18
Pranay Valson <pranay.valson@gmail.com>
2023-03-15 15:39:58 -0700

    update prod db call
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -85,7 +85,7 @@ class DBManager(threading.Thread):
                             )
                         else:
                             cur.execute(
-                                r"SELECT * FROM reports.proof_chain_moonbeam WHERE observer_chain_session_start_block_id > %s AND observer_chain_finalization_tx_hash IS NULL;",
+                                r'SELECT * FROM chain_moonbeam_mainnet."_proof_chain_events" WHERE observer_chain_session_start_block_id > %s AND observer_chain_finalization_tx_hash IS NULL;',
                                 (self.last_block_id,),
                             )
                         outputs = cur.fetchall()
@@ -110,7 +110,7 @@ class DBManager(threading.Thread):
                             )
                         else:
                             cur.execute(
-                                r"SELECT * FROM reports.proof_chain_moonbeam WHERE observer_chain_session_start_block_id > %s;",
+                                r'SELECT * FROM chain_moonbeam_mainnet."_proof_chain_events"  WHERE observer_chain_session_start_block_id > %s;',
                                 (self.last_block_id,),
                             )
 
@@ -150,7 +150,7 @@ class DBManager(threading.Thread):
                         )
                     else:
                         cur.execute(
-                            r"SELECT observer_chain_session_start_block_id FROM reports.proof_chain_moonbeam WHERE observer_chain_finalization_tx_hash IS NULL LIMIT 1"
+                            r'SELECT observer_chain_session_start_block_id FROM chain_moonbeam_mainnet."_proof_chain_events" WHERE observer_chain_finalization_tx_hash IS NULL LIMIT 1'
                         )
                     block_id = cur.fetchone()
             if block_id is not None:
