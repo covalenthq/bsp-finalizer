42fcd33c8807bc042d0a76c009d72a4aca62ccc2
Pranay Valson <pranay.valson@gmail.com>
2023-12-14 14:49:33 -0800

    update moonbase proofchain brp mat view
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- sql/proof_chain_mbase_result.sql
+++ sql/proof_chain_mbase_result.sql
@@ -11,12 +11,12 @@ session_started_events AS (
   JOIN chain_moonbeam_moonbase_alpha.block_transactions trx
     ON (trx.block_id = session_started.block_id AND trx.tx_offset = session_started.tx_offset)
   WHERE
-    session_started.sender = '\x06A38f70c8e245e1cfe1b7d5ACa1B7cd29380343'::bytea
+    session_started.sender = '\x3f91F5034dACc2961F62dCF744286540C8786710'::bytea
     AND session_started.topics @> ARRAY[
       '\x8b1f889addbfa41db5227bae3b091bd5c8b9a9122f874dfe54ba2f75aabe1f4c'::bytea
     ]
     AND trx.successful = TRUE
-    AND session_started.block_id >= '1910104892088990000'::bigint
+    AND session_started.block_id >= '1965606530391765401'::bigint
   ORDER BY session_started.block_id ASC, session_started.log_offset ASC
 ),
 result_reward_awarded_events AS (
@@ -28,10 +28,10 @@ result_reward_awarded_events AS (
   JOIN chain_moonbeam_moonbase_alpha.block_transactions trx_1
     ON (trx_1.block_id = fin.block_id AND trx_1.tx_offset = fin.tx_offset)
   WHERE
-    fin.sender = '\x06A38f70c8e245e1cfe1b7d5ACa1B7cd29380343'::bytea
+    fin.sender = '\x3f91F5034dACc2961F62dCF744286540C8786710'::bytea
     AND fin.topics @> ARRAY['\x93dcf9329a330cb95723152c05719560f2fbd50e215c542854b27acc80c9108d'::bytea]
     AND trx_1.successful = TRUE
-    AND fin.block_id >= '1910104892088990000'::bigint
+    AND fin.block_id >= '1965606530391765401'::bigint
   ORDER BY fin.block_id ASC, fin.log_offset ASC
 ),
 result_quorum_not_reached_events AS (
@@ -43,10 +43,10 @@ result_quorum_not_reached_events AS (
   JOIN chain_moonbeam_moonbase_alpha.block_transactions trx_1
     ON (trx_1.block_id = fin.block_id AND trx_1.tx_offset = fin.tx_offset)
   WHERE
-    fin.sender = '\x06A38f70c8e245e1cfe1b7d5ACa1B7cd29380343'::bytea
+    fin.sender = '\x3f91F5034dACc2961F62dCF744286540C8786710'::bytea
     AND fin.topics @> ARRAY['\x31d16d882c6405d327fa305ecf0d52b45154868e0828822533fd2547f4b21a75'::bytea]
     AND trx_1.successful = TRUE
-    AND fin.block_id >= '1910104892088990000'::bigint
+    AND fin.block_id >= '1965606530391765401'::bigint
   ORDER BY fin.block_id ASC, fin.log_offset ASC
 ),
 all_finalization_events AS (
@@ -67,6 +67,6 @@ LEFT JOIN all_finalization_events afe ON (
   sse.origin_chain_id = afe.origin_chain_id
   AND sse.origin_chain_block_height = afe.origin_chain_block_height
 )
-WHERE sse.origin_chain_block_height > 17635725::numeric
+WHERE sse.origin_chain_block_height > 18781735::numeric
 ORDER BY sse.observer_chain_block_id ASC, sse.observer_chain_tx_offset ASC
 ;
